a1442a0167c315b259f81bce90f19f66a3c45834
Oleg <oefrager@yandex.ru>
2025-02-09 23:58:35 +0400

    02

--- 08-ansible-03-yandex/run_hw.sh
+++ 08-ansible-03-yandex/run_hw.sh
@@ -3,7 +3,7 @@
 echo
 echo "Запуск контейнеров"
 docker run -dit --name clickhouse-01 --rm pycontribs/centos:7 sleep 6000000
-docker run -dit --name vector --rm pycontribs/ubuntu:latest sleep 6000000
-docker run -dit --name lighthouse --rm pycontribs/ubuntu:latest sleep 6000000
+docker run -dit --name vector-02 --rm pycontribs/ubuntu:latest sleep 6000000
+docker run -dit --name lighthouse-03 --rm pycontribs/ubuntu:latest sleep 6000000
 
 docker ps
